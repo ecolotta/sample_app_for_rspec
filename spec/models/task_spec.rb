8fc9b6342df506fd7c3d31513f0edf6651371b78
ecolotta <ecolotta@gmail.com>
2021-01-31 18:46:03 +0900

    CH02 fix:エラーメッセージの確認を追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,16 +4,19 @@ RSpec.describe Task, type: :model do
   it "タイトルが空欄のときエラーになること" do
     task = FactoryBot.build(:task, title: "")
     expect(task).to_not be_valid
+    expect(task.errors[:title]).to eq ["can't be blank"]
   end
 
   it "タイトルが重複するときエラーになること" do
     task1 = FactoryBot.create(:task, title: "title1")
     task2 = FactoryBot.build(:task, title: "title1")
     expect(task2).to_not be_valid
+    expect(task2.errors[:title]).to eq ["has already been taken"]
   end
 
   it "ステータスが空欄のときエラーになること" do
     task = FactoryBot.build(:task, status: "")
     expect(task).to_not be_valid
+    expect(task.errors[:status]).to eq ["can't be blank"]
   end
 end

9d34ad05af6a477c92c8668cca46f10c5ee159dc
ecolotta <ecolotta@gmail.com>
2021-01-31 18:21:02 +0900

    CH02

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:title) { |n| "Title #{n}" }
+    sequence(:content) { |n| "Content #{n}" }
+    status { :todo }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "タイトルが空欄のときエラーになること" do
+    task = FactoryBot.build(:task, title: "")
+    expect(task).to_not be_valid
+  end
+
+  it "タイトルが重複するときエラーになること" do
+    task1 = FactoryBot.create(:task, title: "title1")
+    task2 = FactoryBot.build(:task, title: "title1")
+    expect(task2).to_not be_valid
+  end
+
+  it "ステータスが空欄のときエラーになること" do
+    task = FactoryBot.build(:task, status: "")
+    expect(task).to_not be_valid
+  end
 end
